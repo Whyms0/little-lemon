ccf3b91c51e3567c109aa007fc43745f300133e5
Whyms0 <yanisahraoui055@gmail.com>
2025-06-03 23:51:56 -0400

    im done styling ig

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import '../App.css'
 import './stylesheets/About.css'
+import manager from '../assets/chef3.jpg'
+import manager2 from '../assets/chef.jpg'
+
 const About = () => {
   return (
-    <div>About</div>
+    <article className='about-article'>
+      <div className='about-container'>
+
+      <div className='about-text'>
+        <div className='text-header'>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+        </div>
+        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+      </div>
+
+
+      <div className='about-imgs'>
+      <img className='manager1' src={manager2} alt='Little lemon restaurant managers making food' />
+      <img className='manager2' src={manager} alt='Little lemon restaurant managers making food' />
+      </div>
+      </div>
+    </article>
   )
 }
 
