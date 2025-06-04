2cf369f0e9e4f11fd1ad1f51156137d11cb82b23
Whyms0 <yanisahraoui055@gmail.com>
2025-06-03 22:24:37 -0400

    only about section left

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import '../App.css'
+import './stylesheets/About.css'
 const About = () => {
   return (
     <div>About</div>
--- src/components/Testemonials.js
+++ src/components/Testemonials.js
@@ -1,8 +1,83 @@
 import React from 'react'
-
+import './stylesheets/Testemonials.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserTie, faStar } from '@fortawesome/free-solid-svg-icons';
+import '../App.css'
 const Testemonials = () => {
   return (
-    <div>Testemonials</div>
+    <article className='testemonials-article'>
+      <div className='test-header'>
+        <h2>Testimonials</h2>
+      </div>
+
+      <div className='ratings-container'>
+
+
+        <div className='ratingBox'>
+          <div className='rating'>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <div className='user'>
+            <FontAwesomeIcon icon={faUserTie} />
+            <h3>Yani Sahraoui</h3>
+          </div>
+          <p>Best experience i ever had</p>
+        </div>
+
+        <div className='ratingBox'>
+          <div className='rating'>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <div className='user'>
+            <FontAwesomeIcon icon={faUserTie} />
+            <h3>Julieth Bourgeois</h3>
+          </div>
+          <p>Good service, tasty food</p>
+        </div>
+
+
+        <div className='ratingBox'>
+          <div className='rating'>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <div className='user'>
+            <FontAwesomeIcon icon={faUserTie} />
+            <h3>Fahd Moummadi</h3>
+          </div>
+          <p>W restaurant!</p>
+        </div>
+
+
+        <div className='ratingBox'>
+          <div className='rating'>
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <div className='user'>
+            <FontAwesomeIcon icon={faUserTie} />
+            <h3>Yahya</h3>
+          </div>
+          <p>Love the food!</p>
+        </div>
+
+
+
+      </div>
+    </article>
   )
 }
 
